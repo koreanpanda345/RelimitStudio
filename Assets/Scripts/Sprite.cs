f6a4e04b3079a71ae95993f9f6b2fbb092749847
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2019-03-01 10:34:59 -0800

    Create Sprite.cs
    Add this for sprites

---
+++ Assets/Scripts/Sprite.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+public class Sprite : MonoBehaviour
+{
+  //the animator Method is going to be used to mess with sprite sheets
+    private Animator animator;
+ 
+    // Use this for initialization
+    void Start()
+    {
+    //This is calling on the Animator component on the object
+        animator = this.GetComponent<Animator>();
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        //Input to move
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+        //This is going to start the sprite sheet animation that will be used.
+        if (vertical > 0)
+        {
+            animator.SetInteger("Direction", 2);
+        }
+        else if (vertical < 0)
+        {
+            animator.SetInteger("Direction", 0);
+        }
+        else if (horizontal > 0)
+        {
+            animator.SetInteger("Direction", 1);
+        }
+        else if (horizontal < 0)
+        {
+            animator.SetInteger("Direction", 3);
+        }
+    }
+}
